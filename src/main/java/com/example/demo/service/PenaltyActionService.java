3295f4c1017afd8a6d1ae1630045f5572dbb9f2e
bala-amypo <bala@amypo.in>
2025-12-23 05:43:01 +0000

    User commit

--- src/main/java/com/example/demo/service/PenaltyActionService.java
+++ src/main/java/com/example/demo/service/PenaltyActionService.java
@@ -1,10 +1,22 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.PenaltyAction;
+import org.springframework.stereotype.Service;   
+
 import java.util.List;
+
 @Service
-public interface PenaltyActionService {
-    PenaltyAction addPenalty(PenaltyAction penalty);
-    PenaltyAction getPenaltyById(Long id);
-    List<PenaltyAction> getAllPenalties();
+public class PenaltyActionService {
+
+    public PenaltyAction addPenalty(PenaltyAction p) {
+        return p;
+    }
+
+    public PenaltyAction getPenaltyById(Long id) {
+        return null;
+    }
+
+    public List<PenaltyAction> getAllPenalties() {
+        return List.of();
+    }
 }
