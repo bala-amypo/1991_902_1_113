57a77ae38a1dd4118f944a3abad8a9c5224fe4f5
bala-amypo <bala@amypo.in>
2025-12-23 05:40:01 +0000

    User commit

--- src/main/java/com/example/demo/service/PenaltyActionService.java
+++ src/main/java/com/example/demo/service/PenaltyActionService.java
@@ -2,7 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.entity.PenaltyAction;
 import java.util.List;
-
+@Service
 public interface PenaltyActionService {
     PenaltyAction addPenalty(PenaltyAction penalty);
     PenaltyAction getPenaltyById(Long id);
