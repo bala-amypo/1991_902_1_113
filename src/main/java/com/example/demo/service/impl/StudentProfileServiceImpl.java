7a21caadf2bed93da79b398f8c66b3e6887270d3
bala-amypo <bala@amypo.in>
2025-12-23 07:40:01 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/StudentProfileServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.StudentProfile;
+import com.example.demo.service.StudentProfileService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentProfileServiceImpl implements StudentProfileService {
+
+    @Override
+    public StudentProfile createStudent(StudentProfile profile) {
+        return profile;
+    }
+
+    @Override
+    public StudentProfile getStudentById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<StudentProfile> getAllStudents() {
+        return List.of();
+    }
+
+    @Override
+    public void updateRepeatOffenderStatus(Long studentId) {
+        // temporary logic
+    }
+}
