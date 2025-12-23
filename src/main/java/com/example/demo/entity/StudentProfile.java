bd17762ffbe09eb06989ab5ddccd27ea26e5705e
bala-amypo <bala@amypo.in>
2025-12-23 08:15:10 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentProfile.java
+++ src/main/java/com/example/demo/entity/StudentProfile.java
@@ -1,44 +1,73 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
-import java.time.LocalDateTime;
-
-@Entity
 public class StudentProfile {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(unique = true)
     private String studentId;
-
     private String name;
-
-    @Column(unique = true)
     private String email;
-
     private String program;
-    private Integer yearLevel;
-    private Boolean isRepeatOffender = false;
-    private LocalDateTime createdAt;
-
-    @PrePersist
-    void onCreate() {
-        createdAt = LocalDateTime.now();
+    private int yearLevel;
+    private boolean isRepeatOffender;
+    public StudentProfile() {
     }
 
-    public Long getId() { return id; }
-    public String getStudentId() { return studentId; }
-    public void setStudentId(String studentId) { this.studentId = studentId; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getProgram() { return program; }
-    public void setProgram(String program) { this.program = program; }
-    public Integer getYearLevel() { return yearLevel; }
-    public void setYearLevel(Integer yearLevel) { this.yearLevel = yearLevel; }
-    public Boolean getIsRepeatOffender() { return isRepeatOffender; }
-    public void setIsRepeatOffender(Boolean isRepeatOffender) { this.isRepeatOffender = isRepeatOffender; }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public int getYearLevel() {
+        return yearLevel;
+    }
+
+    public void setYearLevel(int yearLevel) {
+        this.yearLevel = yearLevel;
+    }
+
+
+    public boolean isRepeatOffender() {
+        return isRepeatOffender;
+    }
+
+   
+    public void setRepeatOffender(boolean repeatOffender) {
+        this.isRepeatOffender = repeatOffender;
+    }
 }
