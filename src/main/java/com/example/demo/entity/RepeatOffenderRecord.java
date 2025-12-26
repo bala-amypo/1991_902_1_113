ae1c756a022d20c5ee5b0832c6acd0a41b1c6b5a
bala-amypo <bala@amypo.in>
2025-12-26 08:51:10 +0000

    User commit

--- src/main/java/com/example/demo/entity/RepeatOffenderRecord.java
+++ src/main/java/com/example/demo/entity/RepeatOffenderRecord.java
@@ -19,6 +19,10 @@ public class RepeatOffenderRecord {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Integer getTotalCases() {
         return totalCases;
     }
