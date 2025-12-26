bf74374d067f7697e7716f7459fc6c4859dd3f74
bala-amypo <bala@amypo.in>
2025-12-26 08:00:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/RepeatOffenderRecordRepository.java
+++ src/main/java/com/example/demo/repository/RepeatOffenderRecordRepository.java
@@ -6,8 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RepeatOffenderRecordRepository extends JpaRepository<RepeatOffenderRecord, Long> {
-
-    Optional<RepeatOffenderRecord> findByStudentProfileId(Long studentProfileId);
-
     Optional<RepeatOffenderRecord> findByStudentProfile(StudentProfile studentProfile);
 }
