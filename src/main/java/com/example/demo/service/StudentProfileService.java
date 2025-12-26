c078302c508ca14d3ce5ae0653e766aa27eaf697
bala-amypo <bala@amypo.in>
2025-12-26 06:33:10 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentProfileService.java
+++ src/main/java/com/example/demo/service/StudentProfileService.java
@@ -1,10 +1,15 @@
 package com.example.demo.service;
 
+import java.util.List;
 import com.example.demo.entity.StudentProfile;
 
 public interface StudentProfileService {
 
-    StudentProfile saveProfile(StudentProfile profile);
+    StudentProfile createStudent(StudentProfile profile);
+
+    StudentProfile getStudentById(Long id);
+
+    List<StudentProfile> getAllStudents();
 
     StudentProfile updateRepeatOffenderStatus(Long studentProfileId);
 }
